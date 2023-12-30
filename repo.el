c5666aea22258a38dc4e27a2e122878ad442342c
donjuanplatinum <donjuan@donjuan.lecturify.net>
2023-12-30 20:21:36 +0800

    删除repo.el

--- repo.el
+++
@@ -1,4 +0,0 @@
-(setq package-archives '(("gnu-donjuan"   . "http://127.0.0.1:9998/elpa/gnu/")
-			 ("melpa-donjuan"  . "http://127.0.0.1:9998/elpa/melpa/")
-			 ("nongnu-donjuan" . "http://127.0.0.1/elpa/nongnu/" )
-			 ))
