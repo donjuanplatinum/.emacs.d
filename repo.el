3699c62678ab84e881416af289e5ba6476a4def2
donjuanplatinum <donjuan@donjuan.lecturify.net>
2023-12-30 20:08:32 +0800

    添加历史文件和选择区替换

---
+++ repo.el
@@ -0,0 +1,4 @@
+(setq package-archives '(("gnu-donjuan"   . "http://127.0.0.1:9998/elpa/gnu/")
+			 ("melpa-donjuan"  . "http://127.0.0.1:9998/elpa/melpa/")
+			 ("nongnu-donjuan" . "http://127.0.0.1/elpa/nongnu/" )
+			 ))
