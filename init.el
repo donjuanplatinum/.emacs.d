32b8b0c7d1118765e82d0f3af5d0d1b9d3d3ffa4
donjuanplatinum <donjuan@donjuan.lecturify.net>
2023-12-30 19:37:16 +0800

    初始化

---
+++ init.el
@@ -0,0 +1,7 @@
+;;镜像设置
+(setq package-archives '(("gnu-donjuan"   . "http://127.0.0.1:9998/elpa/gnu/")
+			 ("melpa-donjuan"  . "http://127.0.0.1:9998/elpa/melpa/")
+			 ("nongnu-donjuan" . "http://127.0.0.1/elpa/nongnu/" )
+			 ))
+
+
