57b3f781c09bea5412800e5ba61a3df4325d2c2b
donjuanplatinum <donjuan@donjuan.lecturify.net>
2023-12-31 11:25:58 +0800

    add maple-translate

--- init.el
+++ init.el
@@ -1,11 +1,9 @@
-;;镜像设置
-(setq package-archives '(("gnu-donjuan"   . "http://127.0.0.1:9998/elpa/gnu/")
-			 ("melpa-donjuan"  . "http://127.0.0.1:9998/elpa/melpa/")
-			 ("nongnu-donjuan" . "http://127.0.0.1/elpa/nongnu/" )
-			 ))
+;;关闭提示音
+(setq ring-bell-function 'ignore)
 
 ;;关闭自动备份~文件
 (setq make-backup-files nil)
+(setq auto-save-default nil)
 
 ;;查看所有包的数量
 ;;(length package-alist)
@@ -27,3 +25,33 @@
 ;;主题
 (load-theme 'deeper-blue' 1)
 
+
+;;镜像设置
+(add-to-list 'load-path "~/.emacs.d/lisp/")
+(require 'repo)
+
+
+(use-package maple-translate
+  :ensure nil
+  :commands (maple-translate maple-translate+)
+  )
+
+;;package-install
+(package-install 'use-package)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+
+;;maple-translate
+(add-to-list 'load-path (expand-file-name "~/.emacs.d/site-lisp/maple-translate/"))
+(require 'maple-translate-sdcv)
---
+++ lisp/repo.el
@@ -0,0 +1,6 @@
+;;镜像设置
+(setq package-archives '(("gnu-donjuan"   . "http://127.0.0.1:9998/elpa/gnu/")
+			 ("melpa-donjuan"  . "http://127.0.0.1:9998/elpa/melpa/")
+			 ("nongnu-donjuan" . "http://127.0.0.1/elpa/nongnu/" )
+			 ))
+(provide 'repo)
