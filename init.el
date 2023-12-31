fe32d16de50ff172c2ca6f7a2d965b0c66bb9fe0
donjuanplatinum <donjuan@donjuan.lecturify.net>
2023-12-31 13:37:31 +0800

    add rust

--- init.el
+++ init.el
@@ -59,12 +59,17 @@
 
 ;; To disable shortcut "jump" indicators for each section, set
 (setq dashboard-show-shortcuts nil)
+
+;;rust
+(package-install 'rust-mode)
+
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(sdcv dashboard)))
+ '(package-selected-packages '(rust-mode sdcv dashboard)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
